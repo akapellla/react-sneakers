b322b434760aa3e9fa71ac20f640db5798128839
akapellla <alexnevod12@gmail.com>
2026-01-09 00:17:43 +0300

    Adding new feature:drawer block

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,75 @@ import { useState } from "react";
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer d-flex flex-column">
+          <h2 className="mb-30">Корзина</h2>
+          <div className="cartItems d-flex flex-column">
+            <article className="cartItem d-flex p-20">
+              <img
+                className="sneakers mr-20"
+                src="/img/nike_airmax.jpg"
+                width={70}
+                height={70}
+                alt="sneakers"
+              />
+              <div className="itemDesc">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <div className="bottomItemDesc d-flex align-center justify-between">
+                  <b>12 999 руб.</b>
+                  <button>
+                    <img
+                      className="removeBtn"
+                      height={32}
+                      width={32}
+                      src="/img/deleteBtn.svg"
+                      alt=""
+                    />
+                  </button>
+                </div>
+              </div>
+            </article>
+
+            <article className="cartItem d-flex p-20">
+              <img
+                className="sneakers mr-20"
+                src="/img/puma_x_aka.jpg"
+                width={70}
+                height={70}
+                alt="sneakers"
+              />
+              <div className="itemDesc">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <div className="bottomItemDesc d-flex align-center justify-between">
+                  <b>12 999 руб.</b>
+                  <button>
+                    <img
+                      className="removeBtn"
+                      height={32}
+                      width={32}
+                      src="/img/deleteBtn.svg"
+                      alt=""
+                    />
+                  </button>
+                </div>
+              </div>
+            </article>
+          </div>
+          <ul className="cartTotalBlock">
+            <li className="">
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li className="">
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="" />
@@ -37,6 +106,10 @@ function App() {
 
           <div className="cards d-flex flex-wrap">
             <article className="card">
+              <div className="favorite">
+                <img src="/img/unlikedheart.svg" alt="Unliked" />
+              </div>
+
               <img
                 width={133}
                 height={112}
@@ -111,7 +184,7 @@ function App() {
                 </button>
               </div>
             </article>
-            <article className="card">
+            {/* <article className="card">
               <img
                 width={133}
                 height={112}
@@ -128,7 +201,7 @@ function App() {
                   <img width={11} height={11} src="/img/plus.svg" alt="" />
                 </button>
               </div>
-            </article>
+            </article> */}
           </div>
         </section>
       </main>
