851b9966b9cff68aa7d0fc9224db3eccfcd23233
akapellla <alexnevod12@gmail.com>
2026-01-09 14:45:27 +0300

    implemented a shopping cart block

--- src/App.jsx
+++ src/App.jsx
@@ -57,18 +57,46 @@ function App() {
               </div>
             </article>
           </div>
-          <ul className="cartTotalBlock">
-            <li className="">
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 руб.</b>
-            </li>
-            <li className="">
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
+          <div className="cartTotalBlock">
+            <ul className="list">
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+
+            <button className="placeOrder">
+              <span>Оформить заказ</span>
+              <svg
+                width="16"
+                height="14"
+                viewBox="0 0 16 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 7H14.7143"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8.71436 1L14.7144 7L8.71436 13"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
 
